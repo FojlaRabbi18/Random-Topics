b6659a1803fe5ffa0b077834124d96fb145e46ef
Fojla Rabbi <fojlarabbi018@gmail.com>
2025-11-13 19:12:15 +0600

    Create Number of Subarray with XOR 0.cpp

---
+++ Bitmask/Number of Subarray with XOR 0.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n + 1);
+    for(int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    vector<int> pre(n + 1); 
+    for(int i = 1; i <= n; i++) {
+        pre[i] = pre[i - 1] ^ a[i];
+    }
+    map<int, int> mp; 
+    mp[pre[0]]++; // Very important 
+    int ans = 0;
+    for(int i = 1; i <= n; i++) {
+        ans += mp[pre[i]];
+        mp[pre[i]]++;
+    }
+    cout << ans << '\n';
+
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
