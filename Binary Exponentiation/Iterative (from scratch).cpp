7346de19cbfb89e4342ee20181b6be9daa56bbdd
Fojla Rabbi <fojlarabbi018@gmail.com>
2025-11-14 18:32:13 +0600

    Update Iterative (from scratch).cpp

--- Binary Exponentiation/Iterative (from scratch).cpp
+++ Binary Exponentiation/Iterative (from scratch).cpp
@@ -59,30 +59,3 @@ int main() {
 }
 // 908727740
 
-
-// More optimization
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long
-
-int power(int x, ll n, int mod) { // O(log n) = 64
-    int cur = x % mod; // x^(2^0)   
-    int ans = 1 % mod;
-    while(n > 0) {
-        if(n & 1) {
-            ans = 1LL * ans * cur % mod;
-        }
-        cur = 1LL * cur * cur % mod;
-        n = n >> 1; // Another way of traversal 
-    }
-    return ans;
-  
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout << power(1e8 + 9, 1e18, 1e9 + 7);
-    return 0;
-}
-// 908727740
